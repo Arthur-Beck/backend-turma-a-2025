ef957c784e621a763914625effd22ab66435f32c
Arthur-Beck <beckarthur107@gmail.com>
2025-03-19 13:57:56 +0000

    Update Payment Controller

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,9 +1,16 @@
 import { z } from "zod"
 
+const paymentSchema = z.object({
+    data: z.string().datetime(),
+    valor: z.number().positive(),
+    numero: z.number().int().positive(),
+    observacao: z.string().optional()
+});
+
 const PaymentSchema = z.object({
-    nome: z.string().min(3,{message: "Nome Invaldoi"}),
-    nome: z.string().email({message: "Email Invaldoi"}),
-    nome: z.string().min(6,{message: "senha Invaldoi"})
+    nome: z.string().min(3,{message: "Nome Invaldo"}),
+    nome: z.string().email({message: "Email Invaldo"}),
+    nome: z.string().min(6,{message: "senha Invalda"})
     
 }); 
 
@@ -30,6 +37,23 @@ const paymentController = {
             res.status(500).json({message: error.mesage});
             
         }
+    },
+
+    async updatePayment(req, res) {
+        try {
+            const {id} = req.params;
+            const {valor, numero, data, observacao} = req.body;
+            PaymentSchema.parse({valor, numero, data, observacao});
+            res.status(200).json({message: 'Payment update sucessfuly', data: {id, valor, numero, data, observacao}});
+        } catch (error) {
+            if (error instanceof z.ZodEror) {
+               return res.status(400).json({message: "Validation error",details: error, errors});
+           return  res.satus(500).json({ message: error.mesage });
+        }
+        
+            
+        
+    }
     }
 }
 
